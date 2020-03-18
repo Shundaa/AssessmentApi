98337c5922d0bcdb0d4254870b38640683a9d31a
Cesar <cesar_batt@hotmail.com>
2020-03-18 13:47:03 -0300

    fix validation if

--- src/main/java/com/wipro/api/services/ValidationService.java
+++ src/main/java/com/wipro/api/services/ValidationService.java
@@ -11,22 +11,26 @@ import com.wipro.api.entity.Message;
 @Service
 public class ValidationService {
 	public ResponseEntity<String> validation(Message message) {
-		//messageType: String, 4 chars, alphabetic only. Valid values: AReq, ARes, RReq, RRes, PReq, PRes, Erro.
-        //deviceChannel: String, 2 chars, numeric only. Valid values: 01, 02 and 03.
-        //messageCategory: String, 2 char, numeric only. Valid values: 01 and 02.
-		
-		if(		  (message.getDeviceChannel()==null
-				||Pattern.matches("[0]{1}[1-3]{1}",message.getDeviceChannel()))
-				&&(message.getMessageCategory()==null
-				||Pattern.matches("[0]{1}[1-2]{1}",message.getMessageCategory()))
-				&&(message.getMessageType()==null
-				||(Pattern.matches("[ARP]{1}[R]{1}[e]{1}[qs]{1}",message.getMessageType())
-				||Pattern.matches("Erro",message.getMessageType())))
-				)
-			return new ResponseEntity<>("{\"message\":\"Validation ok\"}",HttpStatus.OK);
-		
+		if (deviceChannelValidation(message) && messageCategoryValidation(message) && messageTypeValidation(message))
+			return new ResponseEntity<>("{\"message\":\"Validation ok\"}", HttpStatus.OK);
+
 		return  new ResponseEntity<>(
-				 "{\"Message\":\"Error\","
-				+"\"Error detail\":\"Data validation error\"}",HttpStatus.NOT_ACCEPTABLE);
+			       "{\"Message\":\"Error\"," +"\"Error code:\":\"4002\","
+			      +"\"Error detail\":\"Data validation error\"}",HttpStatus.NOT_ACCEPTABLE);
+
+	}
+
+	private boolean messageTypeValidation(Message message) {
+		return message.getMessageType() == null
+				|| (Pattern.matches("[ARP]{1}[R]{1}[e]{1}[qs]{1}", message.getMessageType())
+						|| Pattern.matches("Erro", message.getMessageType()));
+	}
+
+	private boolean messageCategoryValidation(Message message) {
+		return message.getMessageCategory() == null || Pattern.matches("[0]{1}[1-2]{1}", message.getMessageCategory());
+	}
+
+	private boolean deviceChannelValidation(Message message) {
+		return message.getDeviceChannel() == null || Pattern.matches("[0]{1}[1-3]{1}", message.getDeviceChannel());
 	}
 }
